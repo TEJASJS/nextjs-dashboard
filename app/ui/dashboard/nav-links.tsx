3f69be1176785272a2f0be3e857accf2d23a362d
TEJASJS <145230086+TEJASJS@users.noreply.github.com>
2025-06-10 08:17:23 +0530

    Update nav-links.tsx
    a changed to Link

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -29,7 +29,7 @@ export default function NavLinks() {
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
-          </a>
+          </Link>
         );
       })}
     </>
